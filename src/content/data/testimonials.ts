23c1e5b311ffc8f7668d914e13658a33b84926e4
ilamuhil <ilamuhil@gmail.com>
2025-11-29 08:39:31 +0530

    fix(testimonial and layout shift): fix testimonials data and mobile layout shift
    fix: layout shift on services page mobile screen
    fix: images on testimonials
    refactor: testimonials data into separate file

---
+++ src/content/data/testimonials.ts
@@ -0,0 +1,25 @@
+const testimonials = [
+  {
+    name: 'Aditya Tyagi',
+    role: 'PHD Applicant',
+    image: '/images/aditya.jpeg',
+    feedback:
+      'I hired Arun for editing academic writing. He did a tremendous job by making my writing effective and more lucid. While doing my work, he also gave me valuable pointers to make my writing more concise without losing the core meaning of it. Highly recommended.',
+  },
+  {
+    name: 'John Fen Raju',
+    role: 'Psychotherapist',
+    image: '/images/john.jpeg',
+    feedback:
+      'I would highly recommend Arun as a linguist with exceptional expertise in academic writing and qualitative research. I have had the privilege of collaborating with Arun on numerous occasions, and I can attest to his outstanding skills and professionalism. Arun possesses a remarkable skill set and deep expertise in academic writing and qualitative research, which have greatly benefited me personally in my own research endeavours. His contributions have consistently elevated the quality of my work. What sets Arun apart is not only his expertise, but also his unwavering commitment to punctuality, integrity, and superior quality in all aspects of his work. He consistently delivers results on time, demonstrating a strong sense of reliability and accountability.',
+  },
+  {
+    name: 'Author',
+    role: 'MDPI',
+    image: '/images/arun.webp',
+    feedback:
+      'We sincerely thank [Arun] for significantly improving the quality of our manuscript, which greatly contributed to its acceptance.',
+  },
+]
+
+export default testimonials
